7ef2969680c9600fcb2c3a2fcd030af4b325aa77
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-12-19 17:04:08 -0500

    Push code modifications

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
     def welcome_email
       @user = params[:user]
-      @login_url = 'https://foodfetchy.onrender.com/login'
+      @login_url = 'https://foodfetch.onrender.com/login'
       mail(to: @user.email, subject: "Welcome to Food Fetchy!")
     end
   end

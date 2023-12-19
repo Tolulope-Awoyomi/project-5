586786eedf789725173f8763f786d623f46ce2bc
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-12-19 18:11:54 -0500

    Resolve duplicate seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_19_045805) do
+ActiveRecord::Schema.define(version: 2023_12_19_230837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
